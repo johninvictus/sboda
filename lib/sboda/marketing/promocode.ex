507e5417d5623a5835bcea92c7de9c939db83777
johninvictus <invictusnitude@gmail.com>
2019-05-21 14:17:11 +0300

    completed promocode struct

--- lib/sboda/marketing/promocode.ex
+++ lib/sboda/marketing/promocode.ex
@@ -11,6 +11,8 @@ defmodule Sboda.Marketing.Promocode do
 
   alias Sboda.Money
 
+  @type_currency_include ~w(KES UGX)
+
   schema "promocodes" do
     field :title, :string
     # where the event is located
@@ -27,7 +29,10 @@ defmodule Sboda.Marketing.Promocode do
     # expir virtual field
     field :expir_str, :string, virtual: true
     # worth string()
+    # formart should be 0.00 KSH
     field :worth_str, :string, virtual: true
+    # Include currency (either KES or UGX)
+    field :currency, :string, virtual: true
 
     timestamps()
   end
@@ -35,12 +40,26 @@ defmodule Sboda.Marketing.Promocode do
   @doc false
   def changeset(promocode, attrs) do
     promocode
-    |> cast(attrs, [:title, :lat, :logt, :expir_str, :worth_str, :active, :distance])
+    |> cast(attrs, [:title, :lat, :logt, :expir_str, :worth_str, :active, :distance, :currency])
     # exclude active since it has a default value
-    |> validate_required([:title, :lat, :logt, :expir_str, :worth_str, :active, :distance])
+    |> validate_required([
+      :title,
+      :lat,
+      :logt,
+      :expir_str,
+      :worth_str,
+      :active,
+      :distance,
+      :currency
+    ])
+    # make sure either KES or UGX is included
+    |> validate_inclusion(:currency, @type_currency_include)
+    # should be 0.00
+    |> validate_format(:worth_str, ~r/\A\d+\.\d{2}\Z/, message: "money format is invalid")
     |> create_point()
+    |> create_naive_date()
+    |> create_money()
   end
-
 
   # This functions takes in latitude and longitude and converts them into a Geo.Point
   defp create_point(changeset) do
@@ -51,6 +70,36 @@ defmodule Sboda.Marketing.Promocode do
       |> put_change(:point, %Geo.Point{coordinates: {data.logt, data.lat}, srid: 4326})
     else
       changeset
+    end
+  end
+
+  # This function convert a valid date string to Naive date which is easier to work with.
+  defp create_naive_date(changeset) do
+    if(changeset.valid?) do
+      data = changeset |> apply_changes()
+
+      case NaiveDateTime.from_iso8601(data.expir_str) do
+        {:ok, naive} ->
+          changeset |> put_change(:expir, naive)
+
+        {:error, :invalid_format} ->
+          changeset
+      end
+    else
+      changeset
+    end
+  end
+
+  # This function converts currency and money string to money struct
+  defp create_money(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true} ->
+        data = changeset |> apply_changes()
+        m_ob = Money.new("#{data.worth_str} " <> "#{data.currency}")
+        put_change(changeset, :worth_str, m_ob)
+
+      %Ecto.Changeset{valid?: false} ->
+        changeset
     end
   end
 end
--- test/sboda/marketing_test.exs
+++ test/sboda/marketing_test.exs
@@ -6,59 +6,6 @@ defmodule Sboda.MarketingTest do
   describe "promocodes" do
     alias Sboda.Marketing.Promocode
 
-    @valid_attrs %{title: "some title"}
-    @update_attrs %{title: "some updated title"}
-    @invalid_attrs %{title: nil}
-
-    def promocode_fixture(attrs \\ %{}) do
-      {:ok, promocode} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Marketing.create_promocode()
-
-      promocode
-    end
-
-    test "list_promocodes/0 returns all promocodes" do
-      promocode = promocode_fixture()
-      assert Marketing.list_promocodes() == [promocode]
-    end
-
-    test "get_promocode!/1 returns the promocode with given id" do
-      promocode = promocode_fixture()
-      assert Marketing.get_promocode!(promocode.id) == promocode
-    end
-
-    test "create_promocode/1 with valid data creates a promocode" do
-      assert {:ok, %Promocode{} = promocode} = Marketing.create_promocode(@valid_attrs)
-      assert promocode.title == "some title"
-    end
-
-    test "create_promocode/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Marketing.create_promocode(@invalid_attrs)
-    end
-
-    test "update_promocode/2 with valid data updates the promocode" do
-      promocode = promocode_fixture()
-      assert {:ok, %Promocode{} = promocode} = Marketing.update_promocode(promocode, @update_attrs)
-      assert promocode.title == "some updated title"
-    end
-
-    test "update_promocode/2 with invalid data returns error changeset" do
-      promocode = promocode_fixture()
-      assert {:error, %Ecto.Changeset{}} = Marketing.update_promocode(promocode, @invalid_attrs)
-      assert promocode == Marketing.get_promocode!(promocode.id)
-    end
-
-    test "delete_promocode/1 deletes the promocode" do
-      promocode = promocode_fixture()
-      assert {:ok, %Promocode{}} = Marketing.delete_promocode(promocode)
-      assert_raise Ecto.NoResultsError, fn -> Marketing.get_promocode!(promocode.id) end
-    end
-
-    test "change_promocode/1 returns a promocode changeset" do
-      promocode = promocode_fixture()
-      assert %Ecto.Changeset{} = Marketing.change_promocode(promocode)
-    end
+    
   end
 end
