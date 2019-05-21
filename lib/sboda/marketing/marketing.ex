f785963ec812dad840456531329598ac891b070e
johninvictus <invictusnitude@gmail.com>
2019-05-21 15:31:51 +0300

    created a function to create promocodes

--- lib/sboda/marketing/marketing.ex
+++ lib/sboda/marketing/marketing.ex
@@ -7,4 +7,21 @@ defmodule Sboda.Marketing do
   alias Sboda.Repo
 
   alias Sboda.Marketing.Promocode
+
+  @doc """ 
+  Creates the promocode when given a Map.
+  
+  # Example
+    iex> Sboda.Marketing.create_promocode(%{ all data here})
+      {:ok, %Sboda.Marketing.Promocode{}}
+
+    iex> Sboda.Marketing.create_promocode(%{ all data here})
+       {:error, %Ecto.Changeset{}}
+  """
+
+  def create_promocode(attr \\ Map.new()) do
+    %Promocode{}
+    |> Promocode.changeset(attr)
+    |> Repo.insert
+  end
 end
